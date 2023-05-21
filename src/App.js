a2c90f7248f156d729a0e88f7166891de14d82df
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2023-05-21 19:40:38 -0400

    🎉 Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { useState } from 'react';
 function App() {
+  const [checked, setChecked] =  useState (false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="">
+        <h1 className={`lg:text-8xl text-5xl duration-500 transition-all font-bold ${checked ? "text-red-500" : ""}`}>An eye for an eye</h1>
+        <label className="swap lg:text-[20rem] text-9xl  py-10">
+  
+  {/* <!-- this hidden checkbox controls the state --> */}
+  <input type="checkbox" onChange={(e) =>  setChecked(e.target.checked)} value={checked}/> 
+  <div className="swap-on"><FaEyeSlash /></div>
+  <div className="swap-off"><FaEye /></div>
+</label>
+      </div>
     </div>
+  </div>
   );
 }
 
